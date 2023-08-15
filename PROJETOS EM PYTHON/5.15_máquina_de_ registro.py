13fb7b41914122302cd6d6f9a6bd6c755b6fb9d5
Samuel Alves <alvess7221@gmail.com>
2023-08-15 16:02:09 -0300

    commit 7

---
+++ PROJETOS EM PYTHON/5.15_máquina_de_ registro.py
@@ -0,0 +1,27 @@
+# c = código 
+# x = preço
+# q = quantidade
+print("-="*30)
+pagar = 0
+while True:
+    c = int(input("Digite o código do produto de: "))
+    preço = 0
+    if c == 1:
+        preço += 0.5
+    elif c == 2:
+        preço += 1
+    elif c == 3:
+        preço += 4
+    elif c == 5:
+        preço += 7
+    elif c == 9:
+        preço += 8
+    elif c == 0:
+        break
+    else:
+        print("Código inválido!")
+    if preço != 0:
+        quantidade = int(input("Quantidade: "))
+        pagar = pagar + (preço * quantidade)
+print("-="*30)
+print(f"O total de compras será R${pagar:.2f}")
