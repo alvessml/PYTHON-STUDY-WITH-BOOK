9ac22b8efa31a719a18d3c751b6d31432055d6e7
Samuel Alves <alvess7221@gmail.com>
2023-08-24 17:06:31 -0300

    commit 15.3

--- AULAS EM PYTHON/AULA5.5.py
+++ AULAS EM PYTHON/AULA5.5.py
@@ -4,4 +4,5 @@ print(f"Price: R${price:>10.2f}!")
 print(f"Price: R${price:^10.2f}!")
 print(f"Price: R${price:_^10.2f}!")  # Put characters between empty spaces
 print(f"Full price: R${int(price):_^10}!") # Functions inside the f-string
-print(f"Price: R${(price * 10):>10.2f}!")
+print(f"Price: R${int(price * 10):>10.2f}!") # Perform mathematical operations
+print(f"""The price is R${price:_^10.2f}.\nAnd you can buy lots of sweets!""")
