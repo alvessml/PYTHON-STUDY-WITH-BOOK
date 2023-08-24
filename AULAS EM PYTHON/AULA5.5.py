c380de4810a22d01985ac8c12e13d97743594149
Samuel Alves <alvess7221@gmail.com>
2023-08-24 16:24:38 -0300

    commit15.1

--- AULAS EM PYTHON/AULA5.5.py
+++ AULAS EM PYTHON/AULA5.5.py
@@ -2,3 +2,4 @@ price = float(input("Enter a price: "))
 print(f"Price: R${price:<10.2f}!")
 print(f"Price: R${price:>10.2f}!")
 print(f"Price: R${price:^10.2f}!")
+print(f"Price: R${price:_^10.2f}!")  # Put characters between empty spaces
