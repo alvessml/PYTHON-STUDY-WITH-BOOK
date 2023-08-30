0b33c62702d52ef2f13f9800964318061b659f11
Samuel Alves <alvess7221@gmail.com>
2023-08-30 16:31:35 -0300

    commit 17 - 30/08/2023

---
+++ PROJETOS EM PYTHON/6.2_lendo_duas_listas_e_gerando_outra.py
@@ -0,0 +1,18 @@
+l1 = []
+l2 = []
+while True:
+    e = int(input("Digite um número para a lista 1 (0 para sair): "))
+    if e == 0:
+        break
+    l1.append(e)
+while True:
+    e = int(input("Digite um número para a lista 2 (0 para sair): "))
+    if e == 0:
+        break
+    l2.append(e)
+l3 = l1[:]
+l3.extend(l2)
+x = 0
+while x < len(l3):
+    print(f"{x}: {l3[x]}")
+    x += 1
