299bd492ef1646ce429bbd26546bb3b40004ee5b
Samuel Alves <alvess7221@gmail.com>
2023-08-30 16:49:14 -0300

    commit 17.1 - 30/08/2023

---
+++ PROJETOS EM PYTHON/6.3_ler_duas_lista_e_imprimir_uma_terceira_sem_repeticões.py
@@ -0,0 +1,12 @@
+l1 = []
+l2 = []
+while True:
+    e = int(input("Digite um número para a primeira lista (0 para sair): "))
+    if e == 0:
+        break
+    l1.append(e)
+while True:
+    e = int(input("DIgite um número para a segunda lista (0 para sair): "))
+    if e == 0:
+        break
+    l2.append(e)
