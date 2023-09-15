99e933b4462eca7e7c2f901061dd93f8e4d8a999
Samuel Alves <alvess7221@gmail.com>
2023-09-15 16:53:00 -0300

    commit 20 - 15/09/2023

--- AULAS EM PYTHON/Programa6.7-Simulação_de_uma_fila_de_banco.py
+++ AULAS EM PYTHON/Programa6.7-Simulação_de_uma_fila_de_banco.py
@@ -1,5 +1,5 @@
 último = 10
-fila = list(range(1, último, + 1))
+fila = list(range(1, último + 1))
 while True:
     print(f"\nExistem {len(fila)} clientes na fila")
     print(f"Fila atual: {fila}")
@@ -8,8 +8,20 @@ while True:
           [A] Para realizar um atendimento;
           [S] Para sair.""")
     operação = input("Digite (F, S ou A): ")
-    if operação == A:
+    if operação == "A":
         if len(fila) > 0:
             atendido = fila.pop(0)
-        print....
-
+            print("-=" * 30)
+            print(f"Cliente {atendido} atendido")
+            print("-=" * 30)
+        else:
+            print("Fila vazia! Ninguém para atender.")   
+    elif operação == "F":
+        último += 1 #(Incrementa o ticket do novo cliente)
+        fila.append(último)
+    elif operação == "S":
+        break
+    else:
+        print("-=" * 30)
+        print("Operação inválida! Digite apenas F, A ou S!")
+        print("-=" * 30)
