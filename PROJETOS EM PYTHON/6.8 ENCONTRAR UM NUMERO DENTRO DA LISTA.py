22bf223cae6ea004b4da66efd0413b0658e929b9
Samuel Alves <alvess7221@gmail.com>
2023-12-18 10:00:36 -0300

    commit 26.1 - 18/12/2023

--- PROJETOS EM PYTHON/6.8 ENCONTRAR UM NUMERO DENTRO DA LISTA.py
+++ PROJETOS EM PYTHON/6.8 ENCONTRAR UM NUMERO DENTRO DA LISTA.py
@@ -1,3 +1,21 @@
 from random import randint
 L = [randint(1, 20) for _ in range(10)]
-print(L)
+
+sem_repetição = set(L)
+
+lista_sem_repetição = list(sem_repetição)
+
+print(lista_sem_repetição)
+
+num = int(input("Digite um número: "))
+
+x = 0 
+while x < len(lista_sem_repetição):
+    if num == lista_sem_repetição[x]:
+        break
+    x += 1
+
+if x < len(list(sem_repetição)):
+    print(f"{num} achado na posição {x}")
+else:
+    print(f"{num} não achado!")
