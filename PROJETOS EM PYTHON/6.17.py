dd37158fa4d85099058ef4e211fda73f56bd0a96
Samuel Alves <alvess7221@gmail.com>
2023-12-27 09:25:30 -0300

    commit 32 - 27/12/2023

--- PROJETOS EM PYTHON/6.17.py
+++ PROJETOS EM PYTHON/6.17.py
@@ -3,38 +3,48 @@ def comercio():
                "alface": [500, 0.45],
                "batata": [200, 1.20],
                "feijão": [100, 8.60]}
-    print("""
+    print(f"""
     ----COMÉRCIO-----
-    [1] Tomate: R$2,30
-    [2] Alface: R$0,45
-    [3] Batata: R$1,20
-    [4] Feijão: R$8,60
+    [1] Tomate: R$2,30 - estoque {estoque["tomate"][0]}
+    [2] Alface: R$0,45 - estoque {estoque["alface"][0]}
+    [3] Batata: R$1,20 - estoque {estoque["batata"][0]}
+    [4] Feijão: R$8,60 - estoque {estoque["feijão"][0]}
     [5] Sair
-          """)
+            """)
 
     compra = []
     while True:
-        produto = int(
-            input("Digite o némero do produto desejado [1][2][3][4] ou [5] para sair: "))
-        if produto == 5:
+        produto = int(input("Digite o némero do produto desejado [1][2][3][4] ou [5] para sair: "))
+        if produto >= 5:
             break
         quantidade = int(input("Quantidade: "))
-        compra.append([produto, quantidade])
+
+        if produto == 1:
+            compra.append(["tomate", quantidade])
+        elif produto == 2:
+            compra.append(["alface", quantidade])
+        elif produto == 3:
+            compra.append(["batata", quantidade])
+        elif produto == 4:
+            compra.append(["feijão", quantidade])
 
     return estoque, compra
 
 
 def processo(estoque, compra):
-    total = 0
+
     print("-=" * 30)
     print("\nVendas realizada: \n")
+
+    total = 0
     for venda in compra:
         produto, quantidade = venda
-        preco = estoque[1]
+        preco = estoque[produto][1]
         custo = preco * quantidade
-        print(f"{produto}: {quantidade} x R${preco:.2f} = R${custo:.2f}")
-        estoque[produto - 1][0] -= quantidade
+        print(f"{quantidade} x R${preco:.2f} = R${custo:.2f}")
+        estoque[produto][0] -= quantidade
         total += custo
+    print("\n")
     return estoque
 
 
@@ -42,10 +52,12 @@ def main():
     estoque, compra = comercio()
     estoque = processo(estoque, compra)
 
+    print("Estoque disponível no momento:")
+
     for chave, dados in estoque.items():
         print(f"Descrição: {chave}")
         print(f"Quantidade: {dados[0]}")
-        print("Preço: %.2f\n" % dados[1])
+        print("Preço: {:.2f} \n".format(dados[1]))
 
 
 main()
