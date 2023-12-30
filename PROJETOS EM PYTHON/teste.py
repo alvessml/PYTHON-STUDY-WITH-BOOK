f230dc978fbfbf0a653a1f00558d343f50ee8b9a
Samuel Alves <alvess7221@gmail.com>
2023-12-30 15:52:44 -0300

    commit 33.1 - 30/12/2023

--- PROJETOS EM PYTHON/teste.py
+++ PROJETOS EM PYTHON/teste.py
@@ -1,17 +1,5 @@
-L = [7,4,3,12,8]
-fim = 5 
-while True: 
-    trocou = False 
-    x=0 
-    while x < (fim-1): 
-        if L[x] > L[x+1]: 
-            trocou = True 
-            temp=L[x] 
-            L[x]=L[x+1]
-            L[x+1]=temp
-        x+=1
-    if not trocou: 
-        break
-    False
-for e in L:
-    print(e)
+L = list("Alô Mundo")
+L[0] = "a"
+print(L)
+s = "".join(L)
+print(s)
