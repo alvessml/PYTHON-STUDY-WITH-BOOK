477a6764c0b162fa54ed04a5e0f511159187cf1a
Samuel Alves <alvess7221@gmail.com>
2023-12-31 11:49:47 -0300

    commit 34.3 - 31/12/2023

--- PROJETOS EM PYTHON/7.2 ENCONTRA CARACTERES COMUNS DE DUAS STRINGS.py
+++ PROJETOS EM PYTHON/7.2 ENCONTRA CARACTERES COMUNS DE DUAS STRINGS.py
@@ -1,3 +1,14 @@
 s1 = input("Digite uma string qualquer: ")
 s2 = input("Digite outra string para comparar com a de cima: ")
 
+s3 = ""
+for letra in s1:
+    # SE CADA LETRA DA STRING 1 ESTIVER NA SEGUNDA STRING E NÃO ESTIVER
+    # NA TERCEIRA STRING, SERÁ ADICIONADO NA TERCEIRA STRING!
+    if letra in s2 and letra not in s3:
+        s3 += letra
+
+if s3 == "":
+    print("Nenhum caracteres em comum encontrado!")
+else:
+    print(f"Caracteres em comuns são: {s3}.")
