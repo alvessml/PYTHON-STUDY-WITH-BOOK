5a0a7dd67bf292484f26469b5b388070dfa349f6
Samuel Alves <alvess7221@gmail.com>
2024-01-05 09:29:38 -0300

    commit 38.2 - 05/01/2024

--- AULAS EM PYTHON/7.10 JOGO DA FORCA.py
+++ AULAS EM PYTHON/7.10 JOGO DA FORCA.py
@@ -31,3 +31,22 @@ while True:
     # DESENHO DO BONECO DA FORCA!
     print("X==:==\nX  :  ")
     print("X  O  " if erros >= 1 else "X")
+    linha2 = ""
+    if erros == 2:
+        linha2 = "  |  "
+    elif erros == 3:
+        linha2 = " /|  "
+    elif erros >= 4:
+        linha2 = " /|\ "
+    print("X%s" % linha2)
+
+    linha3 = ""
+    if erros == 5:
+        linha3 += " /   "
+    elif erros >= 6:
+        linha3 += " / \ "
+    print("X%s" % linha3)
+    print("X\n==========")
+    if erros == 6:
+        print("Enforcado!!!")
+        break
