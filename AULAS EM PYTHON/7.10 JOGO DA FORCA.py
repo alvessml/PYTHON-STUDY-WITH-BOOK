11c253e5837df174967b898bc703568999c79bc4
Samuel Alves <alvess7221@gmail.com>
2024-01-06 11:21:42 -0300

    commit 39 - 06/01/2024

--- AULAS EM PYTHON/7.10 JOGO DA FORCA.py
+++ AULAS EM PYTHON/7.10 JOGO DA FORCA.py
@@ -1,25 +1,34 @@
 palavra = input("Digite a palavra secreta: ").lower().strip()
 
-for x in range(100):
+for x in range(50):
     print()
-
+print("--"*30)
 digitadas = []
 acertos = []
-erros = []
+erros = 0
 while True:
     senha = ""
     for letra in palavra:
-        senha += letra if letra in acertos else "."
-    print(senha)
 
+        # VAI IGUAL A MINHA LETRA SE EU ACERTA A LETRA DA SENHA, SE NÃO IRÁ PERMANECER OS " . ". #
+        senha += letra if letra in acertos else "."
+    print(f"senha: {senha}")
+
+    # SE MINHA SENHA ESTIVER IGUAL A PALAVRA, ENTÃO EU ACERTEI TODAS! #
     if senha == palavra:
+        print("--"*30)
         print("Você acertou!")
         break
 
-    tentativa = input("\n Digite uma letra: ").lower().strip()
+    # COMEÇO DA FOCA, APÓS DIGITAR A PALAVRA PARA ADIVINHAR. #
+    tentativa = input("\nDigite uma letra: ").lower().strip()
     if tentativa in digitadas:
         print("Você já tentou esta letra!")
         continue
+
+    # CÓDIGO PARA SABER SE A MINHA TENTATIVA ESTÁ EM PALVRA, SE SIM
+    # ELE VAI JOGAR PARA MINHA LISTA ACERTOS! Se não irá jogar para
+    # a lsita de erros.
     else:
         digitadas += tentativa
         if tentativa in palavra:
@@ -28,12 +37,13 @@ while True:
             erros += 1
             print("Você erro!")
 
-    # DESENHO DO BONECO DA FORCA!
+    # DESENHO DO BONECO DA FORCA.
     print("X==:==\nX  :  ")
     print("X  O  " if erros >= 1 else "X")
     linha2 = ""
     if erros == 2:
         linha2 = "  |  "
+
     elif erros == 3:
         linha2 = " /|  "
     elif erros >= 4:
