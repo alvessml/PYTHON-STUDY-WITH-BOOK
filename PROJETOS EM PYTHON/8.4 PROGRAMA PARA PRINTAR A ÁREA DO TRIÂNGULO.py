bab558ba1907dce72ef9da2da0260662d0e0db7d
Samuel Alves <alvess7221@gmail.com>
2024-01-12 10:08:04 -0300

    commit 43.3 - 12/01/2024

---
+++ PROJETOS EM PYTHON/8.4 PROGRAMA PARA PRINTAR A ÁREA DO TRIÂNGULO.py
@@ -0,0 +1,8 @@
+def área(a, b):
+    return (a * b) / 2
+
+b = int(input("Digite a base do triângulo: "))
+
+h = int(input("Digite a altura do triângulo: "))
+
+print(f"A área do triângulo é {área(b, h):.0f}")
