1abccfe70d16c14e1a7ef07dc29faae0e57c1138
Samuel Alves <alvess7221@gmail.com>
2024-01-13 15:38:40 -0300

    commit 44.3 - 13/01/2024

---
+++ PROJETOS EM PYTHON/8.6 SOMA.py
@@ -0,0 +1,8 @@
+def soma(L):
+    soma_total = 0
+    for e in L:
+        soma_total += e
+    return soma_total
+
+L = [1,7,2,9,15]
+print(soma(L))
--- PROJETOS EM PYTHON/teste.py
+++ PROJETOS EM PYTHON/teste.py
@@ -1,9 +1,1 @@
-def fatorial(x):
-    fat = 1
-    while x > 1:
-        fat *= x
-        x -= 1
-    return fat
 
-
-print(fatorial(5))
