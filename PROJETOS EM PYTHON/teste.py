19b288c0fe46bf86a072b675cae03ba1d7139bdf
Samuel Alves <alvess7221@gmail.com>
2024-01-24 09:37:00 -0300

    commit 53.1 - 24/01/2024

--- PROJETOS EM PYTHON/teste.py
+++ PROJETOS EM PYTHON/teste.py
@@ -1,10 +1,1 @@
-ESPAÇOS_POR_NÍVEL = 4
-
-def imprime_elementos(l, nível = 0)
-    espaços = " " * ESPAÇOS_POR_NÍVEL * nível
-    
-    if type(l) == list:
-        print(espaços, "[")
-        for e in l:
-            imprime_elementos(e, nível=1)
-            
+          
