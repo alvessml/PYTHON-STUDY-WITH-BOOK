174ac4ede6991208ed060421946270ca3bc6175e
Samuel Alves <alvess7221@gmail.com>
2024-01-26 09:37:07 -0300

    commit 55.2 - 26/01/2024

--- PROJETOS EM PYTHON/9.1.1 RECEBER O NOME DO ARQUIVO E IMPRIMIR CADA LINHA.py
+++ PROJETOS EM PYTHON/9.1.1 RECEBER O NOME DO ARQUIVO E IMPRIMIR CADA LINHA.py
@@ -1,16 +1,10 @@
 import sys
 
-# Verifica se o parâmetro foi passado
-if len(sys.argv) != 2:  # Lembre-se que o nome do programa é o primeiro da lista
-    print("\nUso: e09-01.py nome_do_arquivo\n\n")
+if len(sys.argv) != 2:
+    print("\nUso: e09-01.py nom_do_arquivo\n\n")
 else:
-    nome = sys.argv[1]
+    nome = sys.arg[1]
     arquivo = open(nome, "r")
     for linha in arquivo.readlines():
-        # Como a linha termina com ENTER,
-        # retiramos o último caractere antes de imprimir
         print(linha[:-1])
     arquivo.close()
-
-# Não esqueça de ler sobre encodings
-# Dependendo do tipo de arquivo e de seu sistema operacional,
--- PROJETOS EM PYTHON/teste.py
+++ PROJETOS EM PYTHON/teste.py
@@ -1,1 +1,4 @@
-          
+import sys
+
+if "win" in sys.platform:
+    print(sys.platform)
