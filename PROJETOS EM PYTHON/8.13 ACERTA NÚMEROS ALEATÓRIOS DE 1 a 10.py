24d325b759255ec93cb0b8a6f81fb76899ac379c
Samuel Alves <alvess7221@gmail.com>
2024-02-11 10:44:21 -0300

    commit 56 - 11/02/2024

--- PROJETOS EM PYTHON/8.13 ACERTA NÚMEROS ALEATÓRIOS DE 1 a 10.py
+++ PROJETOS EM PYTHON/8.13 ACERTA NÚMEROS ALEATÓRIOS DE 1 a 10.py
@@ -1,5 +1,20 @@
-from random import randint 
-L = [randint(1, 10) for _ in range(10)]
+import random
+
+x = random.sample(range(1, 11), 3)
+
+print(x)
+
+erro = 0
 while True:
     n = int(input("Digite um número para advinhar os que estão na lista gerada: "))
-    if n
+    
+    if erro == 2:
+        break
+    
+    if n == list(x):
+        print("Você acertou!")
+        break
+    else:
+        print("Você errou")
+        erro += 1
+    
